8d96e90dcda3117e3374663e766b191bfbb722ae
Martin Vidner <martin@vidner.net>
2023-05-09 09:57:49 +0200

    Debug prints

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@ export XAUTHORITY="/host/${XAUTHORITY:-$HOME/.Xauthority}"
 dbus-uuidgen --ensure
 for D in /checkout /dbus-gui-gtk; do
     pushd $D
-    rbenv exec bundle exec bin/dbus-gui-gtk
+    rbenv exec sh -c "bundle exec sh -c 'bin/dbus-gui-gtk; echo GUI exit: $?'; echo Bundler exit: \$?"
+    echo Rbenv exit: $?
     test -f Gemfile && break
     popd
 done
